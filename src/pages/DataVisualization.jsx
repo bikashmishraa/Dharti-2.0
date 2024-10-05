6ac49de0b44ecdfaca344a55e402c878b3552fe4
Sijal Manandhar <sijal.m06@gmail.com>
2024-10-06 02:16:11 +0545

    hero background video added

--- src/pages/DataVisualization.jsx
+++ src/pages/DataVisualization.jsx
@@ -1,12 +1,5 @@
-import { LineChart, Line } from 'recharts';
-const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, ];
 const DataVisualization = () => {
-  return <div className='py-28'>
-     <h1>hello</h1>
-     <LineChart width={400} height={400} data={data}>
-    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-  </LineChart>
-  </div>;
+  return <section className="section-wrapper"></section>;
 };
 
 export default DataVisualization;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,39 @@
 import { Link } from "react-router-dom";
+import HomeBg from "@/assets/home-bg.mp4";
 
 const Home = () => {
   return (
-    <section className="section-wrapper flex-center">
-      <div className="hero-content text-neutral-content text-center">
-        <div className="max-w-md space-y-6">
-          <h1 className="text-5xl font-bold">
-            Welcome to <br /> Dharti-2.0
-          </h1>
-          <p>
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+    <>
+      <section className="section-wrapper flex-center relative">
+        <video
+          className="absolute w-auto size-full max-w-none"
+          src={HomeBg}
+          autoPlay
+          muted
+          loop
+        ></video>
+        <span className="absolute inset-0 bg-black size-full bg-opacity-75" />
 
-          <Link to="/about" className="inline-block">
-            <button className="btn btn-primary text-[white]">Learn More</button>
-          </Link>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md space-y-6">
+            <h1 className="text-5xl font-bold">
+              Welcome to <br /> Dharti-2.0
+            </h1>
+            <p>
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+
+            <Link to="/about" className="inline-block">
+              <button className="btn btn-primary text-[white]">
+                Learn More
+              </button>
+            </Link>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
