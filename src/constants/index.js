da08dfa74d9fc9f507182ffade9d29c3a0fff903
Sijal Manandhar <sijal.m06@gmail.com>
2024-10-05 12:46:04 +0545

    changes in hero section

---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+const NAV_LINKS = [
+  { title: "home", ref: "/" },
+  { title: "data visualization", ref: "/data-visualization" },
+  { title: "about", ref: "/about" },
+  { title: "team", ref: "/team" },
+  { title: "contact us", ref: "/contact" },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,7 @@
-import heroImage from "@/assets/hero-image.svg";
+import Hero from "../components/home/Hero";
 
 const Home = () => {
-  return (
-    <div className="hero-bg py-10">
-      <div className="max-w-4xl md:space-y-7 px-5 mx-auto">
-        <section className="hero bg-base-300 rounded-md shadow-md">
-          <div className="text-center">
-            <h1 className="text-5xl font-bold">Hello there</h1>
-            <div className="size-full rounded-xl">
-              <img src={heroImage} alt="" />
-            </div>
-            <button className="btn btn-neutral">Learn More</button>
-          </div>
-        </section>
-      </div>
-    </div>
-  );
+  return <Hero />;
 };
 
 export default Home;
