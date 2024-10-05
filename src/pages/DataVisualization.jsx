d8ef3e5b52b18c88104b4ffbcbce2c61d0de67e0
bikashmishraa <bikash.mishra2079@gmail.com>
2024-10-05 15:35:22 +0545

    added all details

--- src/pages/DataVisualization.jsx
+++ src/pages/DataVisualization.jsx
@@ -1,7 +1,12 @@
 import { LineChart, Line } from 'recharts';
 const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, ];
 const DataVisualization = () => {
-  return <div>DataVisualization</div>;
+  return <div className='py-28'>
+     <h1>hello</h1>
+     <LineChart width={400} height={400} data={data}>
+    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+  </LineChart>
+  </div>;
 };
 
 export default DataVisualization;
