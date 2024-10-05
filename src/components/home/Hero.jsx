2cbd7ee698130483ebacf8e00ec02c2679722e1a
bikashmishraa <bikash.mishra2079@gmail.com>
2024-10-05 14:22:07 +0545

    no change

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -13,14 +13,14 @@ const Hero = () => {
       <div className="hero-content text-neutral-content text-center">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">
-            Welcome to <br /> Dharti-2.0
+            Welcome to <br /> <span className="text-[red]"> Dharti-2.0 </span>
           </h1>
           <p className="mb-5">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary text-[white]">Get Started</button>
         </div>
       </div>
     </div>
