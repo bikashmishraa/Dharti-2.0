29d98ed24a89365dbb01771dcfaf204908212ed1
Sijal Manandhar <sijal.m06@gmail.com>
2024-10-06 08:35:41 +0545

    header tranition, data visualization section title added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,28 @@
 import { NAV_LINKS } from "@/constants";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [scrollY, setScrollY] = useState(0);
+
+  const handleScroll = () => setScrollY(window.scrollY);
+
+  useEffect(() => {
+    function watchScroll() {
+      window.addEventListener("scroll", handleScroll);
+    }
+    watchScroll();
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
   return (
-    <div className="navbar bg-primary text-neutral-content fixed z-10">
+    <div
+      className={`navbar bg-primary text-neutral-content fixed z-10 transition ${
+        scrollY < 120 ? "bg-opacity-10" : "bg-opacity-80"
+      }`}
+    >
       <div className="flex-1">
         <Link to={"/"} className="btn btn-ghost text-3xl font-semibold">
           Dharti
