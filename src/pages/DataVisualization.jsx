25850f352bf2488eb81f5d93169d8cdfc9a356c4
Sijal Manandhar <sijal.m06@gmail.com>
2024-10-06 06:36:33 +0545

    data-visualization section updated

--- src/pages/DataVisualization.jsx
+++ src/pages/DataVisualization.jsx
@@ -1,10 +1,24 @@
-import JupyterEmbed from "../components/JupyterEmbed";
+import CollabData from "@/data/collab/collab.json";
 
 const DataVisualization = () => {
   return (
     <section className="section-wrapper">
-      <h1>DataVisualization</h1>
-      <JupyterEmbed />
+      <div className="space-y-4">
+        {CollabData.map((collab, idx) => (
+          <div key={idx} className="space-y-3">
+            <h3 className="text-neutral-content text-2xl font-medium">
+              {collab.title}
+            </h3>
+            <img src={`/${collab.image}`} alt="" className="rounded-lg" />
+            <a
+              href={collab.url}
+              className="text-neutral-content text-lg hover:underline"
+            >
+              Go to collab
+            </a>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
